6b5a1336c649688e9227b758c3795baaa1e0567d
isalamajor <isabel.hernandez7barrio@gmail.com>
2024-12-05 11:07:56 +0100

    R1, R2 ok

--- CSPMaintenance.py
+++ CSPMaintenance.py
@@ -33,14 +33,22 @@ def load_data(filepath):
     print("parkings: ", parkings)
 
     # En la línea 6 y posteriores se indica la info de los aviones, split nos deja la lista ['1', 'JMB', 'T', '2', '2']
+    aviones_jmb, aviones_std = [], []
     aviones = [line.strip().split('-') for line in lines[5:]] 
+    for avion in aviones:
+        if avion[1] == 'JMB':
+            aviones_jmb.append(avion)
+        else:
+            aviones_std.append(avion)
     print("aviones: ", aviones)
+    print("aviones JMB: ", aviones_jmb)
+    print("aviones STD: ", aviones_std)
     
-    return franjas, matrix_size, talleres_std, talleres_spc, parkings, aviones
+    return franjas, matrix_size, talleres_std, talleres_spc, parkings, aviones, aviones_std, aviones_jmb
 
 def main(filepath):
     # Load data
-    franjas, matriz_size, talleres_std, talleres_spc, parkings, aviones = load_data(filepath)
+    franjas, matriz_size, talleres_std, talleres_spc, parkings, aviones, aviones_std, aviones_jmb = load_data(filepath)
     
     # Variables
     problem = Problem()
@@ -68,46 +76,22 @@ def main(filepath):
     
     
     # Restricción 2: Capacidad de los talleres
-    '''for franja in franjas_horarias:
-        for taller in talleres['STD'] + talleres['SPC']:
+    for franja in franjas_horarias:
+        for taller in talleres_spc + talleres_std:
             def capacity_constraint(*assignments):
                 std_count = sum(1 for avion in aviones  # ['1', 'JMB', 'T', '2', '2']
-                                if f"{avion[0]}_franja_{franja}" in assignments and avion[1] == 'STD')
-                                #if f"{a_id}_franja_{franja}" == taller and tipo == 'STD')
+                                if f"{avion[0]}_franja_{franja}" == taller and avion in aviones_std)
                 jumbo_count = sum(1 for avion in aviones 
-                                if f"{avion[0]}_franja_{franja}" in assignments and avion[1] == 'JMB')
-                                #if f"{a_id}_franja_{franja}" == taller and tipo == 'JMB')
+                                if f"{avion[0]}_franja_{franja}" == taller and avion in aviones_jmb)
                 
                 # Máximo 2 aviones en total, y como máximo 1 de tipo JUMBO
                 return std_count + jumbo_count <= 2 and jumbo_count <= 1
             
-            variables = [f"{avion_id}_franja_{franja}" for avion_id in avion_ids]
-            problem.addConstraint(capacity_constraint, variables)'''
-
-
-
-    # Restricción 2: Capacidad de los talleres
-    
-    for franja in franjas_horarias:
-        def capacity_constraint(*vars):
-            # Contar cuántos aviones 'STD' están asignados al taller y franja actuales
-            std_count = sum(1 for avion in aviones # avion = ['1', 'JMB', 'T', '2', '2']
-                            if f"{avion[0]}_franja_{franja}" == vars and avion[1] == 'STD') # Si se asigna el avión y es STD ??? #TODO
-
-            # Contar cuántos aviones 'JMB' están asignados al taller y franja actuales
-            jmb_count = sum(1 for avion in aviones
-                            if f"{avion[0]}_franja_{franja}" == vars and avion[1] == 'JMB')
-
-            # Restricción: máximo 2 aviones en total, y como máximo 1 JMB
-            return std_count + jmb_count <= 2 and jmb_count <= 1
-
-        # Variables correspondientes a esa franja horaria
-        vars = [f"{avion_id}_franja_{franja}" for avion_id in avion_ids]
-        #problem.addConstraint(capacity_constraint, vars)
-
+            vars = [f"{avion_id}_franja_{franja}" for avion_id in avion_ids]
+            problem.addConstraint(capacity_constraint, vars)
 
     # Restricción 3: Tareas de mantenimiento
-    for avion_id, tipo, restr, t1, t2 in aviones:
+    '''for avion_id, tipo, restr, t1, t2 in aviones:
         t1, t2 = int(t1), int(t2)
         
         if t2 > 0:
@@ -115,53 +99,28 @@ def main(filepath):
             problem.addConstraint(
                 lambda *assignments: sum(1 for a in assignments if a in talleres_spc) >= t2, 
                 vars
-            )
+            )'''
 
-
-    
-    # Restricción 4: las tareas tipo 2 deben hacerse primero si el avión es de rest 'T'
-    def verificar_orden_tareas(asignaciones, t1, t2):
-        """
-        Verifica que las tareas especializadas (tipo 2) se realicen antes que las tareas estándar (tipo 1)
-        """
-        # Las primeras t2 franjas deben ser asignadas a talleres SPC
-        for i in range(t2):
-            if asignaciones[i] not in talleres_spc:
-                return False  # Si alguna tarea SPC no está en un taller SPC, no cumple la restricción
+    for avion in aviones: 
         
-        return True  
-    
-    for avion_id, tipo, restr, t1, t2 in aviones:
-        t1, t2 = int(t1), int(t2) 
+        def tareasSPCenTalleresSPC(*assignments):
+            for taller_spc in talleres_spc:
+
+                talleres_spc_asignados = sum(1 for franja in franjas_horarias
+                    if f"{avion[0]}_franja_{franja}" == taller_spc) # avion[0] es el ID 
+                
+                return talleres_spc_asignados >= int(avion[4]) # t2 del avion (número de tareas tipo 2 spc)
         
-        if restr == 'T' and t2 > 0:  
+        
+        vars = [f"{avion_id}_franja_{franja}" for avion_id in avion_ids]
+        problem.addConstraint(tareasSPCenTalleresSPC, avion)
+        
 
-            vars_avion = [f"{avion_id}_franja_{franja}" for franja in franjas_horarias]
-            
-            problem.addConstraint(
-                verificar_orden_tareas,  
-                vars_avion + [t1, t2]  # Pasamos los talleres SPC, t1, y t2 como argumentos
-            )        
 
-    
-    # Restricción 5 y 6: Maniobrabilidad y adyacencia
-    for franja in franjas_horarias:
-        for taller in talleres['STD'] + talleres['SPC'] + talleres['PRK']:
-            adjacentes = [
-                (taller[0] - 1, taller[1]), (taller[0] + 1, taller[1]),
-                (taller[0], taller[1] - 1), (taller[0], taller[1] + 1)
-            ]
-            adjacentes = [a for a in adjacentes if a in talleres['STD'] + talleres['SPC'] + talleres['PRK']]
-            def adjacency_constraint(*assignments):
-                return all(a not in adjacentes for a in assignments)
-            
-            variables = [f"{avion_id}_franja_{franja}" for avion_id in avion_ids]
-            #problem.addConstraint(adjacency_constraint, variables)
-    
+
     # Solve problem
     print("x")
     solutions = problem.getSolutions()
-    print(solutions)
     print("Ya")
 
     # Output result
